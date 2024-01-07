aed13393cc60819624c0570bcd8c9cac6c34e38c
diwakardayal <diwakardayal65@gmail.com>
2024-01-07 22:22:56 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/** @format */
+"use strict";
+const projectSection = document.getElementById("projectSection") || null;
+const experienceSection = document.getElementById("experienceSection") || null
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("experienceBtn").addEventListener("click", () => {
+    // Add the 'hide' class to the project section
+    projectSection.classList.add("hide");
+    projectSection.classList.remove("show");
+    experienceSection.classList.remove("hide");
+    console.log("hide class added to project section");
+  });
+
+  document.getElementById("projectBtn").addEventListener("click", () => {
+    // Add the 'hide' class to the project section
+    experienceSection.classList.add("hide");
+    experienceSection.classList.remove("show");
+    projectSection.classList.remove("hide");
+  });
+
+  
+});
